11a7de0ea3ce26ec8c4a1fba08c0891e05a42516
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2020-12-04 17:50:16 +0100

    Day three

--- problems/03/part_1.js
+++ problems/03/part_1.js
@@ -1,6 +1,50 @@
-const solve = (input) => {
-  console.log(input);
-  return 'Implement a solution for the input'
+const isTree = (char) => {
+  if (!char) return false;
+  return char === '#';
+}
+
+const isOpenSquare = (char) => {
+  if (!char) return false;
+  return char === '.';
+}
+
+const solve = (rows) => {
+  const trees = [];
+  const currentPosition = {
+    zeroBasedRow: 0,
+    zeroBasedPosition: 0
+  };
+  const positionsToJump = 3;
+  const rowsToJump = 1;
+  const twoDimensionalArray = rows.map(row => [...row]);
+  const maximumPosition = twoDimensionalArray.length * positionsToJump;
+  console.log('maximumPosition', maximumPosition);
+  for (i = 0; i < twoDimensionalArray.length; i++) {
+    currentPosition.zeroBasedPosition = currentPosition.zeroBasedPosition + positionsToJump;
+    currentPosition.zeroBasedRow = i + rowsToJump;
+    let newPositionAfterSlope;
+    let upcomingRow = twoDimensionalArray[currentPosition.zeroBasedRow];
+    if (upcomingRow && upcomingRow[currentPosition.zeroBasedPosition]) {
+      newPositionAfterSlope = upcomingRow[currentPosition.zeroBasedPosition];
+    }
+    
+    if (upcomingRow && !upcomingRow[currentPosition.zeroBasedPosition]) {
+      currentPosition.zeroBasedPosition = currentPosition.zeroBasedPosition + positionsToJump % upcomingRow.length;
+      newPositionAfterSlope = upcomingRow[currentPosition.zeroBasedPosition];
+    }
+
+    if (isTree(newPositionAfterSlope)) {
+      trees.push(newPositionAfterSlope);
+      upcomingRow[currentPosition.zeroBasedPosition] = 'X';
+    }
+
+    if (isOpenSquare(newPositionAfterSlope)) {
+      upcomingRow[currentPosition.zeroBasedPosition] = 'O';
+    }
+  }
+
+  console.log(twoDimensionalArray.map(row => row.join('')));
+  return 'Amount of trees: ' + trees.length;
 }
 
 module.exports = { solve }
--- problems/03/part_2.js
+++ problems/03/part_2.js
@@ -1,6 +1,37 @@
-const solve = (input) => {
-  console.log(input);
-  return 'Implement a solution for the input'
+const isTree = (char) => {
+  if (!char) return false;
+  return char === '#';
+}
+
+const countTrees = (rows, right, down) => {
+  let amountOfTrees = 0;
+  let startPosition = right;
+  for (let i = down; i < rows.length; i += down) {
+    let currentRow = rows[i];
+    const isPositionOutOfBounds = startPosition > currentRow.length - 1;
+    if (isPositionOutOfBounds) {
+      startPosition = startPosition - currentRow.length;
+    }
+
+    const char = currentRow[startPosition];
+    if (isTree(char)) {
+      amountOfTrees++;
+    }
+
+    startPosition += right;
+  }
+
+  return amountOfTrees;
+};
+
+const solve = (rows) => {
+  const first = countTrees(rows, 1, 1);
+  const second = countTrees(rows, 3, 1);
+  const third = countTrees(rows, 5, 1);
+  const fourth = countTrees(rows, 7, 1);
+  const fifth = countTrees(rows, 1, 2);
+  const multiplied = first * second * third * fourth * fifth;
+  return `Multiplied amount: ${multiplied} trees`;
 }
 
 module.exports = { solve }
